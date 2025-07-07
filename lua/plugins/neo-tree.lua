4f6e0618af1b193f41a6e148b95402ec0ec10006
Guillermo Tanamachi <gtortega@amazon.com>
2025-07-07 12:00:28 -0600

    feat: Add hidden files in neo tree

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -7,6 +7,15 @@ return {
     "MunifTanjim/nui.nvim",
   },
   config = function()
+    require("neo-tree").setup({
+      filesystem = {
+        filtered_items = {
+          visible = true,
+          hide_dotfiles = false,
+          hide_gitignored = false,
+        },
+      },
+    })
     vim.keymap.set("n", "<leader>tt", ":Neotree toggle<CR>", { desc = "Toggle tree", noremap = true, silent = true })
   end
 }
