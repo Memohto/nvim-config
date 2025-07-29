f571648648b9cb95e9cadca70b595caa969c9d82
Guillermo Tanamachi <gtortega@amazon.com>
2025-07-29 10:34:43 -0600

    feat: Remap some key bindings + attempt of remote clipboard

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -10,8 +10,9 @@ vim.g.mapleader = " "
 
 -- Key maps
 vim.keymap.set('n', '<leader>w', '<C-w>', { noremap = true })
-vim.keymap.set('n', '<leader>ws', '<C-w>s', { desc = "Split horizontally", noremap = true })
+vim.keymap.set('n', '<leader>wh', '<C-w>s', { desc = "Split horizontally", noremap = true })
 vim.keymap.set('n', '<leader>wv', '<C-w>v', { desc = "Split vertically", noremap = true })
+vim.keymap.set("n", "<leader>wt", function() Snacks.terminal() end, { desc = "Terminal (cwd)" })
 
 vim.keymap.set("n", "<C-h>", ":wincmd h<CR>")
 vim.keymap.set("n", "<C-j>", ":wincmd j<CR>")
@@ -24,7 +25,18 @@ vim.keymap.set("n", "<leader>bd", ":bd<CR>", { desc = "Delete buffer", noremap =
 vim.keymap.set("n", "<leader>bn", ":bn<CR>", { desc = "Next buffer", noremap = true, silent = true })
 vim.keymap.set("n", "<leader>bp", ":bp<CR>", { desc = "Previous buffer", noremap = true, silent = true })
 
-vim.keymap.set("n", "<leader>xx", ":xa<CR>", { noremap = true, silent = true })
-vim.keymap.set({ "n", "i", "v" }, "<C-s>", ":w<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<C-x>", ":xa<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<C-w>", ":w<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<C-q>", ":qa!<CR>", { noremap = true, silent = true })
 
-vim.keymap.set("n", "<leader>fT", function() Snacks.terminal() end, { desc = "Terminal (cwd)" })
+vim.g.clipboard = {
+  name = 'OSC 52',
+  copy = {
+    ['+'] = require('vim.ui.clipboard.osc52').copy('+'),
+    ['*'] = require('vim.ui.clipboard.osc52').copy('*'),
+  },
+  paste = {
+    ['+'] = require('vim.ui.clipboard.osc52').paste('+'),
+    ['*'] = require('vim.ui.clipboard.osc52').paste('*'),
+  },
+}
