7914a5d41432d6e2397820df3ba94353910829eb
Memohto <guillermohiroshi@gmail.com>
2025-09-20 21:54:38 -0600

    feat: markdown, comfy numbers and mini stuff

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -8,18 +8,39 @@ vim.opt.relativenumber = true
 
 vim.g.mapleader = " "
 
+-- Function to toggle line numbers
+local function toggle_line_numbers()
+  if vim.wo.number or vim.wo.relativenumber then
+    vim.wo.number = false
+    vim.wo.relativenumber = false
+    -- turn off snacks indent
+    if Snacks and Snacks.indent then
+      Snacks.indent.disable()
+    end
+  else
+    vim.wo.number = true
+    vim.wo.relativenumber = true
+    -- turn on snacks indent
+    if Snacks and Snacks.indent then
+      Snacks.indent.enable()
+    end
+  end
+end
+
 -- Key maps
 vim.keymap.set("i", "jj", "<Esc>", { noremap = true })
 
-vim.keymap.set('n', '<leader>-', '<C-w>s', { desc = "Split horizontally", noremap = true })
-vim.keymap.set('n', '<leader>|', '<C-w>v', { desc = "Split vertically", noremap = true })
+vim.keymap.set("n", "<leader>n", toggle_line_numbers, { desc = "Toggle Copy Mode" })
+
+vim.keymap.set('n', '<leader>-', '<C-w>s', { desc = "Split Horizontally", noremap = true })
+vim.keymap.set('n', '<leader>|', '<C-w>v', { desc = "Split Vertically", noremap = true })
 
 vim.keymap.set("n", "<C-h>", ":wincmd h<CR>")
 vim.keymap.set("n", "<C-j>", ":wincmd j<CR>")
 vim.keymap.set("n", "<C-k>", ":wincmd k<CR>")
 vim.keymap.set("n", "<C-l>", ":wincmd l<CR>")
 
-vim.keymap.set("n", "<leader>bd", ":bd<CR>", { desc = "Delete buffer", noremap = true, silent = true })
+vim.keymap.set("n", "<leader>bd", ":bd<CR>", { desc = "Delete Buffer", noremap = true, silent = true })
 
 vim.keymap.set("n", "<C-x>", ":xa<CR>", { noremap = true, silent = true })
 vim.keymap.set("n", "<C-w>", ":w<CR>", { noremap = true })
